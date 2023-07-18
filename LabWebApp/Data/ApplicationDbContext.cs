07a192892c01262480fadb1b00f9e718b7f74da5
Matias Moretti <matiassmoretti@gmail.com>
2023-07-17 22:26:42 -0300

    Primeiro Commit

---
+++ LabWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using LabWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+        }
+    }
+}
